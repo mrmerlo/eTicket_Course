d55c16b6bd0632d465ca495b70db0723014adaf6
mrmerlo <mrmerlo@DESKTOP-4SACVSS>
2023-04-14 10:28:23 +0200

    Aggiungere i file di progetto.

---
+++ eTicket_Course/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTicket_Course.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
